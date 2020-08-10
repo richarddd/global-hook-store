76e1c97cc7b641446a98c597237c5d44016c8605
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 13:56:06 +0000

    Deploy richarddd/global-hook-store to github.com/richarddd/global-hook-store.git:gh-pages

---
+++ precache-manifest.12a7507b3257e12571ee54c920ea8c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b18f46656bffbb63ee237694ee1c77",
+    "url": "/global-hook-store/index.html"
+  },
+  {
+    "revision": "88694bf50dfa231d867e",
+    "url": "/global-hook-store/static/css/main.6f694fae.chunk.css"
+  },
+  {
+    "revision": "5b1c013445b97ceb9f31",
+    "url": "/global-hook-store/static/js/2.35844f53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/global-hook-store/static/js/2.35844f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88694bf50dfa231d867e",
+    "url": "/global-hook-store/static/js/main.21360ef2.chunk.js"
+  },
+  {
+    "revision": "a047e4125eaa2374b7dc",
+    "url": "/global-hook-store/static/js/runtime-main.52256daf.js"
+  }
+]);
